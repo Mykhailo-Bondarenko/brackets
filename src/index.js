7e1f935c738feb4df28e15bed493301a3a960ce7
Mykhailo Bondarenko <mikhailzyagun@gmail.com>
2022-07-14 20:13:15 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const array = str.split('');
+  const result = [];
+  const obj = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    obj[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (obj[array[i]]) {
+      if (obj[array[i]] !== result.pop()) return false;
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result.length === 0;
 }
